2130616905cae2849a4dc45856f636b38f109d48
Kartik Nain <kartik.nain.9@gmail.com>
2023-04-09 15:34:28 -0400

    Required routes implemented

--- routes/api/sales.js
+++ routes/api/sales.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { celebrate, Joi, errors, Segments } = require('celebrate');
 
 const Sale = require('./../../models/sale')
 
@@ -8,19 +9,48 @@ router.post('/', (req, res) => {
 
     const saleData = req.body;
     const sale = new Sale(saleData);
-    
+
     sale
         .save()
-        .then(() => {res.status(201).send('Add Success')})
+        .then(() => { res.status(201).send('Add Success') })
         .catch((err) => res.status(500).send(err.message))
 
 })
+
+router.get('/', celebrate({
+    [Segments.QUERY]: {
+        page: Joi.number().integer().required(),
+        perPage: Joi.number().integer().required(),
+        storeLocation: Joi.string()
+    }
+}), (req, res) => {
+    const page = req.query.page
+    const perPage = req.query.perPage
+
+    // calculate the skip value for pagination
+    const skip = (page - 1) * perPage
+
+    // build the query based on the storeLocation parameter, if provided
+    const storeLocation = req.query.storeLocation ? { storeLocation: req.query.storeLocation } : {}
+
+    Sale.find(storeLocation).skip(skip).limit(perPage).then(sale => {
+        if (sale) {
+            res.status(200).send(sale)
+        } else {
+            res.status(404).send("Not Found.")
+        }
+    })
+        .catch(err => res.send(err))
+})
+
+router.use(errors());
+
 
 // Get a document using _id URL : /api/sales/:_id
 router.get('/:id', (req, res) => {
 
     Sale
-        .findOne({_id: req.params.id})
+        .findOne({ _id: req.params.id })
         .then(sale => {
             if (sale) {
                 res.status(200).send(sale)
@@ -38,10 +68,10 @@ router.put('/:id', (req, res) => {
 
     Sale
         .updateOne(
-            {_id: req.params.id},
-            {$set: updates})
+            { _id: req.params.id },
+            { $set: updates })
         .exec()
-        .then(() => {res.status(201).send('Sales Document Updated!')})
+        .then(() => { res.status(201).send('Sales Document Updated!') })
         .catch(err => res.status(500).send(err.message))
 
 })
@@ -50,9 +80,9 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 
     Sale
-        .deleteOne({_id: req.params.id})
+        .deleteOne({ _id: req.params.id })
         .exec()
-        .then(() => {res.status(201).send('Sale Document Deleted!')})
+        .then(() => { res.status(201).send('Sale Document Deleted!') })
         .catch(err => res.status(500).send(err.message))
 
 })
